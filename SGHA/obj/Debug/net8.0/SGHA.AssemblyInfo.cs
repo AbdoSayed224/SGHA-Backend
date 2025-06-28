4a8d1e94fe00217bee71b6d3a92d0cdf378f4676
Karim H. Essa <113004991+Kimovx@users.noreply.github.com>
2025-06-28 19:39:44 +0300

    Added new database  server

--- SGHA/obj/Debug/net8.0/SGHA.AssemblyInfo.cs
+++ SGHA/obj/Debug/net8.0/SGHA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGHA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eba9e63eef505168e849f48ebc0d1aef3aca8d3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a855bf09ae0153ae14060be1d851db37716f624")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGHA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGHA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
